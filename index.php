b12db8803d294734394c1dc2c8d57e5bfd829bc2
Fernanda Dias <diasfeh@gmail.com>
2014-04-29 14:15:07 -0300

    Formulario html

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<h1>Login</h1>
+	<form action="" method="post">
+		<p>Usuario: <input type="text" name="user" id="user"></p>
+		<p>Senha: <input type="password" name="pass" id="key"></p>
+		<input type="submit" value="Logar" id="login">
+		<input type="submit" value="Reset" id="reset">
+	</form>
+</html>
